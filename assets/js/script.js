dc4da9103724481f9ac2fc7241de19ba9435e062
Rodrigo Urrea <rurream@gmail.com>
2020-01-30 16:13:03 -0300

    final 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
       $('[data-toggle="tooltip"]').tooltip()
     });
 
+
+
+
+
+
+
+
+    $(function () {
+      $(".nav_logo").each(function(i) {
+        setTimeout(()=>{$(this).addClass('animated fadeInUp')}, 250 * i);
+    });
+      
+      ;
+      
+  });
+
 });
